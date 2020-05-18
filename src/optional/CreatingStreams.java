703565d7e22eee01f3d26b935854bf4ebd7eace3
yrdavis <yrd1@hotmail.com>
2020-05-17 22:28:47 -0700

    Creating streams exercise

--- src/optional/CreatingStreams.java
+++ src/optional/CreatingStreams.java
@@ -1,32 +1,49 @@
 package optional;
 
+/*
+ * @author Yvonne Davis
+ * @version 1
+ */
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
-import java.util.stream.Stream;
 
 public class CreatingStreams {
 	public static void main(String[] args) {
-		//1. Create a stream out of the following collections.
-		String[] strArr = {"one", "two", "three", "four"};
-		
+		// 1. Create a stream out of the following collections.
+		String[] strArr = { "one", "two", "three", "four" };
+		// String array to List and stream
+		// List<String> strList = Arrays.asList(strArr);
+		// Stream<String> streamOfString = strList.stream();
+		Arrays.stream(strArr);
+
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
 		intList.add(2);
 		intList.add(3);
 		intList.add(4);
-		
+
+		// ArrayList Integer stream
+		intList.stream();
+
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
-		
+
+		// Stack stream
+		randStack.stream();
+
 		ArrayDeque<Double> dQueue = new ArrayDeque<Double>();
 		dQueue.push(0.0);
 		dQueue.push(1.0);
 		dQueue.push(2.0);
 		dQueue.push(3.0);
+
+		// dQeque stream
+		dQueue.stream();
 	}
 }
